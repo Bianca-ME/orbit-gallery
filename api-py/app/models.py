ccdbf843f46c4b4ea2b2620d411e2f2597e8f40e
Bianca-ME <bianca.mgeg@protonmail.com>
2025-10-12 16:17:12 +0200

    Modified api-py files to make FastAPI work. Made first POST andGET. Succesful communication with PostgreSQL.

--- api-py/app/models.py
+++ api-py/app/models.py
@@ -1,4 +1,5 @@
 from sqlalchemy import Column, Integer, String, DateTime, func
+from sqlalchemy.dialects.postgresql import ARRAY
 from .database import Base
 
 class Photo(Base):
@@ -6,6 +7,6 @@ class Photo(Base):
 
     id = Column(Integer, primary_key=True, index=True)
     title = Column(String, nullable=False)
-    tags = Column(String, nullable=True)
+    tags = Column(ARRAY(String), nullable=True)
     s3_key = Column(String, nullable=False)
     created_at = Column(DateTime(timezone=True), server_default=func.now())
--- api-py/app/schemas.py
+++ api-py/app/schemas.py
@@ -1,13 +1,17 @@
 from pydantic import BaseModel
+from typing import Optional, List
 from datetime import datetime
 
 class PhotoCreate(BaseModel):
     title: str
-    tags: str | None = None
+    tags: Optional[List[str]] = None
     s3_key: str
 
 class PhotoResponse(PhotoCreate):
     id: int
+    title: str
+    tags: Optional[List[str]] = None
+    s3_key: str
     created_at: datetime
 
     class Config:
