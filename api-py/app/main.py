21a5d5cc102101ca40be64ed121f515e4b6f246d
Bianca-ME <bianca.mgeg@protonmail.com>
2026-01-01 19:18:48 +0100

    Install MinIO dependency, add MinIO client and upload file to MinIO

--- api-py/app/main.py
+++ api-py/app/main.py
@@ -1,9 +1,17 @@
+from minio import Minio
 import os
 from fastapi import FastAPI, Depends, UploadFile, File
 from sqlalchemy.orm import Session
 from . import models, schemas, database
 
 app = FastAPI()
+
+minio_client = Minio(
+    "minio-orbit:9000",         # minio-orbit = container name (Docker DNS); MiniO API port: 9000
+    access_key="minioadmin",
+    secret_key="minioadmin",
+    secure=False,
+)
 
 # Dependency: DB session
 def get_db():
@@ -30,20 +38,41 @@ def create_photo(photo: schemas.PhotoCreate, db: Session = Depends(get_db)):
 def list_photos(db: Session = Depends(get_db)):
     return db.query(models.Photo).all()
 
-@app.post("/photos/upload-test")
+# @app.post("/photos/upload-test")  # Temporarily save uploaded file inside container
+# async def upload_test(file: UploadFile = File(...)):
+#     upload_dir = "uploads" # to be clear: the dir is created inside the container
+#     os.makedirs(upload_dir, exist_ok=True) # create the dir if it doesn't exist
+
+#     file_path = os.path.join(upload_dir, file.filename)
+
+#     with open(file_path, "wb") as buffer: # create a real file; write binary data
+#         content = await file.read() # read the file content async. (read the file's bytes)
+#         buffer.write(content)
+
+#     return {
+#         "filename": file.filename,
+#         "saved_to": file_path,
+#     }
+
+# run docker `compose exec api-py ls uploads` to see the uploaded files inside the container
+
+@app.post("/photos/upload-test")    # Upload file to MinIO
 async def upload_test(file: UploadFile = File(...)):
-    upload_dir = "uploads" # to be clear: the dir is created inside the container
-    os.makedirs(upload_dir, exist_ok=True) # create the dir if it doesn't exist
+    print(">>> UPLOADING TO MINIO <<<")
+    object_name = file.filename
 
-    file_path = os.path.join(upload_dir, file.filename)
-
-    with open(file_path, "wb") as buffer: # create a real file; write binary data
-        content = await file.read() # read the file content async. (read the file's bytes)
-        buffer.write(content)
+    minio_client.put_object(
+        bucket_name="photos",
+        object_name=object_name,
+        data=file.file,
+        length=-1,
+        part_size=10 * 1024 * 1024,
+        content_type=file.content_type,
+    )
+    print(">>> UPLOAD FINISHED <<<")
 
     return {
         "filename": file.filename,
-        "saved_to": file_path,
+        "stored_in": "minio",
+        "object_name": object_name,
     }
-
-# run docker `compose exec api-py ls uploads` to see the uploaded files inside the container
